e2fc85685d7653c0697a94c1d955080aa3e069e7
Julien Lepiller <julien@lepiller.eu>
2017-03-02 17:37:59 +0100

    build skia and use it in icecat

--- more/packages/google.scm
+++ more/packages/google.scm
@@ -20,15 +20,16 @@
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages)
   #:use-module (gnu packages compression)
-  #:use-module (gnu packages icu4c)
   #:use-module (gnu packages fontutils)
+  #:use-module (gnu packages gl)
   #:use-module (gnu packages gnuzilla)
+  #:use-module (gnu packages icu4c)
   #:use-module (gnu packages image)
   #:use-module (gnu packages libevent)
-  #:use-module (gnu packages python)
-  #:use-module (gnu packages perl)
-  #:use-module (gnu packages ruby)
   #:use-module (gnu packages ninja)
+  #:use-module (gnu packages perl)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages ruby)
   #:use-module (gnu packages xml)
   #:use-module (guix packages)
   #:use-module (guix download)
@@ -76,44 +77,82 @@
           "1x5m8ri6hmj9pbq4amglhkwbkcvhqp4vh8njwlrnlrsaipfdy62c"))
         (modules '((guix build utils)))
         (snippet
-         `(delete-file-recursively "third_party"))))
+         `(begin
+            (chdir "third_party")
+            (delete-file-recursively "expat")
+            (delete-file-recursively "libjpeg-turbo")
+            (delete-file-recursively "freetype2")
+            (delete-file-recursively "libpng")
+            (delete-file-recursively "libwebp")
+            (delete-file-recursively "zlib")
+            (for-each (lambda (dir)
+                        (for-each (lambda (f)
+                                    (mkdir-p (dirname (string-append "externals/" f)))
+                                    (copy-file f (string-append "externals/" f)))
+                                  (find-files dir)))
+                      '("icu" "sfntly"))
+            (chdir "..")
+            (for-each (lambda (f)
+                        (substitute* f
+                          ((".*//third_party/expat.*") "")
+                          ((".*//third_party/freetype2.*") "")
+                          ((".*//third_party/libjpeg-turbo:libjpeg.*") "")
+                          ((".*//third_party/libpng.*") "")
+                          ((".*//third_party/libwebp.*") "")
+                          ((".*//third_party/zlib.*") "")))
+                      '("BUILD.gn" "third_party/dng_sdk/BUILD.gn"))
+            #t))))
     (build-system gnu-build-system)
     (arguments
      `(#:tests? #f
        #:phases
        (modify-phases %standard-phases
          (replace 'configure
-           (lambda* (#:key outputs #:allow-other-keys)
+           (lambda* (#:key inputs #:allow-other-keys)
              (zero? (system*
                       "gn" "gen" "out/Shared"
                       (string-append "--args="
                                      "cc=\"gcc\" "
                                      "cxx=\"g++\" "
+                                     "extra_cflags=[\"-I"
+                                      (assoc-ref inputs "freetype")
+                                      "/include/freetype2"
+                                     "\"]"
                                      "skia_use_sfntly=false "
-                                     "skia_use_system_expat=true "
-                                     "skia_use_system_libjpeg_turbo=true "
-                                     "skia_use_system_freetype2=true "
+                                     "skia_use_dng_sdk=false "
+                                     ;"skia_use_system_expat=true "
+                                     ;"skia_use_system_libjpeg_turbo=true "
+                                     ;"skia_use_system_freetype2=true "
                                      ;"skia_use_system_icu=true "
-                                     "skia_use_system_libpng=true "
-                                     "skia_use_system_libwebp=true "
-                                     "skia_use_system_zlib=true "
+                                     ;"skia_use_system_libpng=true "
+                                     ;"skia_use_system_libwebp=true "
+                                     ;"skia_use_system_zlib=true "
                                      "is_official_build=true "
                                      "is_component_build=true")))))
          (replace 'build
            (lambda* (#:key outputs #:allow-other-keys)
-             (zero? (system* "ninja" "-C" "out/Shared")))))))
+             (zero? (system* "ninja" "-C" "out/Shared"))))
+         (replace 'install
+           (lambda* (#:key outputs #:allow-other-keys)
+              (let ((lib (string-append (assoc-ref outputs "out") "/lib")))
+                (mkdir-p lib)
+                (copy-file "out/Shared/libskia.so"
+                           (string-append lib "/libskia.so"))))))))
     (native-inputs
      `(("gn" ,google-gn)
        ("ninja" ,ninja)
        ("python" ,python-2)))
     (inputs
      `(("expat" ,expat)
+       ("fontconfig" ,fontconfig)
        ("freetype" ,freetype)
-       ("libpng" ,libpng)
-       ("libz" ,zlib)
+       ("glu" ,glu)
        ("icu" ,icu4c)
+       ("libjpeg-turbo" ,libjpeg-turbo)
+       ("libpng" ,libpng)
        ("libwebp" ,libwebp)
-       ("libjpeg-turbo" ,libjpeg-turbo)))
+       ("libz" ,zlib)
+       ("mesa" ,mesa)))
     (home-page "https://skia.org")
     (synopsis "Graphics library")
     (description "Graphics library.")
