d94e8d1d9bb16ea2e9056273660ac680ed285c94
Julien Lepiller <tyreunom@lepiller.eu>
2017-07-30 10:39:12 +0200

    add gupnp and tools

--- more/packages/video.scm
+++ more/packages/video.scm
@@ -20,21 +20,26 @@
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix build-system gnu)
+  #:use-module (guix build-system glib-or-gtk)
+  #:use-module (guix utils)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages)
   #:use-module (gnu packages algebra)
   #:use-module (more packages boost)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages fontutils)
+  #:use-module (gnu packages freedesktop)
   #:use-module (gnu packages gl)
   #:use-module (gnu packages glib)
   #:use-module (gnu packages gnome)
+  #:use-module (gnu packages gtk)
   #:use-module (gnu packages libreoffice) ; hunspell
   #:use-module (gnu packages linux)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages pulseaudio)
   #:use-module (gnu packages video)
   #:use-module (gnu packages wxwidgets)
+  #:use-module (gnu packages xml)
   #:use-module (gnu packages xorg))
 
 (define-public ffms2
@@ -68,7 +73,7 @@ programmers to access a standard API to open and decompress media files")
 
 (define-public aegisub
   (package
-    (name "aegisub")
+    (name "aegisub-wip")
     (version "3.2.2")
     (source (origin
               (method url-fetch)
@@ -88,7 +93,7 @@ programmers to access a standard API to open and decompress media files")
        ;; tests require busted, a lua package we don't have yet
        #:tests? #f))
     (inputs
-     `(("boost" ,boost)
+     `(("boost" ,boost-fix)
        ("desktop-file-utils" ,desktop-file-utils)
        ("ffms2" ,ffms2)
        ("fftw" ,fftw)
@@ -112,3 +117,146 @@ programmers to access a standard API to open and decompress media files")
    ; the the Expat license, with a rewording (Software -> Materials). (called MIT
    ; by upstream). See https://github.com/Aegisub/Aegisub/blob/master/LICENCE
    ; src/MatroskaParser.(c|h) is under bsd-3 with permission from the author
+
+(define-public gssdp
+  (package
+    (name "gssdp")
+    (version "1.0.2")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://download.gnome.org/sources/gssdp/"
+                                  (version-major+minor version) "/gssdp-"
+                                  version ".tar.xz"))
+              (sha256
+               (base32
+                "1p1m2m3ndzr2whipqw4vfb6s6ia0g7rnzzc4pnq8b8g1qw4prqd1"))))
+    (build-system gnu-build-system)
+    (home-page "")
+    (inputs
+     `(("util-linux" ,util-linux)
+       ("glib" ,glib)
+       ("libsoup" ,libsoup)))
+    (native-inputs
+     `(("gobject-introspection" ,gobject-introspection)
+       ("pkg-config" ,pkg-config)
+       ("vala" ,vala)))
+    (synopsis "")
+    (description "")
+    (license license:lgpl2.0)))
+
+(define-public gupnp
+  (package
+    (name "gupnp")
+    (version "1.0.2")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://download.gnome.org/sources/gupnp/"
+                                  (version-major+minor version) "/gupnp-"
+                                  version ".tar.xz"))
+              (sha256
+               (base32
+                "043nqxlj030a3wvd6x4c9z8fjarjjjsl2pjarl0nn70ig6kzswsi"))))
+    (build-system gnu-build-system)
+    (inputs
+     `(("util-linux" ,util-linux)
+       ("glib" ,glib)
+       ("gssdp" ,gssdp)
+       ("libsoup" ,libsoup)
+       ("libxml" ,libxml2)))
+    (native-inputs
+     `(("gobject-introspection" ,gobject-introspection)
+       ("pkg-config" ,pkg-config)
+       ("vala" ,vala)))
+    (home-page "")
+    (synopsis "")
+    (description "")
+    (license license:lgpl2.0)))
+
+(define-public gupnp-av
+  (package
+    (name "gupnp-av")
+    (version "0.12.9")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://download.gnome.org/sources/gupnp-av/"
+                                  (version-major+minor version) "/gupnp-av-"
+                                  version ".tar.xz"))
+              (sha256
+               (base32
+                "0iylhn722rlmnzrmwx452a9ag42v8nk9mrcfdjv9f6ka4m4n9ib2"))))
+    (build-system gnu-build-system)
+    (inputs
+     `(("gssdp" ,gssdp)
+       ("gupnp" ,gupnp)
+       ("glib" ,glib)
+       ("libsoup" ,libsoup)
+       ("libxml" ,libxml2)
+       ("util-linux" ,util-linux)))
+    (native-inputs
+     `(("gobject-introspection" ,gobject-introspection)
+       ("pkg-config" ,pkg-config)
+       ("vala" ,vala)))
+    (home-page "")
+    (synopsis "")
+    (description "")
+    (license license:lgpl2.0)))
+
+(define-public gupnp-dlna
+  (package
+    (name "gupnp-dlna")
+    (version "0.10.5")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://download.gnome.org/sources/gupnp-dlna/"
+                                  (version-major+minor version) "/gupnp-dlna-"
+                                  version ".tar.xz"))
+              (sha256
+               (base32
+                "0spzd2saax7w776p5laixdam6d7smyynr9qszhbmq7f14y13cghj"))))
+    (build-system gnu-build-system)
+    (inputs
+     `(("gssdp" ,gssdp)
+       ("gupnp" ,gupnp)
+       ("glib" ,glib)
+       ("libsoup" ,libsoup)
+       ("libxml" ,libxml2)
+       ("util-linux" ,util-linux)))
+    (native-inputs
+     `(("gobject-introspection" ,gobject-introspection)
+       ("pkg-config" ,pkg-config)
+       ("vala" ,vala)))
+    (home-page "")
+    (synopsis "")
+    (description "")
+    (license license:lgpl2.0)))
+
+(define-public gupnp-tools
+  (package
+    (name "gupnp-tools")
+    (version "0.8.13")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://download.gnome.org/sources/gupnp-tools/"
+                                  (version-major+minor version) "/gupnp-tools-"
+                                  version ".tar.xz"))
+              (sha256
+               (base32
+                "1vbr4iqi7nl7kq982agd3liw10gx67s95idd0pjy5h1jsnwyqgda"))))
+    (build-system glib-or-gtk-build-system)
+    (inputs
+     `(("gssdp" ,gssdp)
+       ("gupnp" ,gupnp)
+       ("gupnp-av" ,gupnp-av)
+       ("gtk3" ,gtk+)
+       ("gtksourceview" ,gtksourceview)
+       ("libsoup" ,libsoup)
+       ("libxml" ,libxml2)
+       ("util-linux" ,util-linux)))
+    (native-inputs
+     `(("gobject-introspection" ,gobject-introspection)
+       ("pkg-config" ,pkg-config)
+       ("vala" ,vala)))
+    (home-page "")
+    (synopsis "")
+    (description "")
+    (license license:lgpl2.0)))
