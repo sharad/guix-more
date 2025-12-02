59fc6bec7fb4b1d246704cb57ab96f9fffe96c85
Sharad Pratap <110339+sharad@users.noreply.github.com>
2025-12-02 12:20:27 +0530

    correction

--- more/packages/binary.scm
+++ more/packages/binary.scm
@@ -34,6 +34,7 @@
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages python)
   #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages python-build)
   #:use-module (gnu packages python-crypto)
   #:use-module (gnu packages qt)
   #:use-module (gnu packages tls)
